f2ecdf481282fe6789ecd368c5eff4c52ebf37ad
Wallace Patrick <patrickwallace621@gmail.com>
2023-10-10 14:55:25 -0300

    User update with PUT

--- src/main/java/com/wallace/workshopmongo/services/UserService.java
+++ src/main/java/com/wallace/workshopmongo/services/UserService.java
@@ -5,9 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import com.wallace.workshopmongo.domain.User;
 import com.wallace.workshopmongo.dto.UserDTO;
@@ -20,28 +17,36 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	@GetMapping
 	public List<User> findAll() {
 		return repository.findAll();
 	}
 	
-	@GetMapping
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
-	@PutMapping
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
 	
-	@DeleteMapping
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
