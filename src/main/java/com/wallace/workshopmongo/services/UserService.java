744d3c2c0e81c71c76e5e4fa6e54e9366044d610
Wallace Patrick <patrickwallace621@gmail.com>
2023-10-10 14:32:23 -0300

    User insertion with POST

--- src/main/java/com/wallace/workshopmongo/services/UserService.java
+++ src/main/java/com/wallace/workshopmongo/services/UserService.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.wallace.workshopmongo.domain.User;
+import com.wallace.workshopmongo.dto.UserDTO;
 import com.wallace.workshopmongo.repository.UserRepository;
 import com.wallace.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +28,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	@PutMapping
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
